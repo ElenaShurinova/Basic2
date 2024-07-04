72927fe13fa6a1a71492862eb70a098370b666c0
dgali <dgalita197@gmail.com>
2024-07-04 17:03:14 +0200

    HW2 v2

--- src/main/java/org/example/homeworks/hw2/Strings.java
+++ src/main/java/org/example/homeworks/hw2/Strings.java
@@ -2,7 +2,7 @@ package org.example.homeworks.hw2;
 
 public class Strings {
     public static void main(String[] args) {
-        String str1 = new String("stroka");
+        String str1 = new String("I study Basic Java!");
 
 
 
